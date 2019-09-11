fbc12b5e8543cf08393625d81b04d7e456007ae4
Michal Majcherski <michal.majcherski@gft.com>
2019-09-11 15:19:07 +0200

    Fixed UserNamesTransformerTest re-run issue on Windows

--- kafka-streams/src/test/java/org/gft/big/data/practoce/kafka/academy/streams/transformations/UserNamesTransformerTest.java
+++ kafka-streams/src/test/java/org/gft/big/data/practoce/kafka/academy/streams/transformations/UserNamesTransformerTest.java
@@ -17,6 +17,7 @@ import org.junit.Test;
 import java.io.IOException;
 import java.util.List;
 import java.util.Properties;
+import java.util.Random;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
 import java.util.stream.Collectors;
@@ -66,8 +67,13 @@ public class UserNamesTransformerTest {
                 })
                 .collect(Collectors.toList());
 
+        /*
+         * KLUDGE: Closing the Test Driver doesn't close the state stores properly. Changing applicationId will work
+         * around this, but will bloat your tmp folder, so be sure to delete it after some runs.
+         */
+        Random random = new Random();
         Properties streamsConfig = new Properties();
-        streamsConfig.put(StreamsConfig.APPLICATION_ID_CONFIG, "UserNamesTransformerTest");
+        streamsConfig.put(StreamsConfig.APPLICATION_ID_CONFIG, "UserNamesTransformerTest" + random.nextInt());
         streamsConfig.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.LongSerde.class.getName());
         streamsConfig.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.StringSerde.class.getName());
         streamsConfig.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
@@ -93,15 +99,13 @@ public class UserNamesTransformerTest {
             testDriver.pipeInput(recordFactory.create(inputTopic, keyValue.key, keyValue.value));
         }
 
-        ProducerRecord<Long, String> output = null;
+        ProducerRecord<Long, String> output;
         do {
             output = testDriver.readOutput(outputTopic, new LongDeserializer(), new StringDeserializer());
             if(output != null) {
                 Assert.assertTrue("Each name must start with an A", output.value().startsWith("A"));
-                Assert.assertTrue("Each entry must be name + pause + surname", output.value().split(" ").length == 2);
+                Assert.assertEquals("Each entry must be name + pause + surname", 2, output.value().split(" ").length);
             }
         } while (output != null);
-
-        testDriver.close();
     }
 }
