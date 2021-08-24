e8f4bcc7d81f03e682f8e113113919abcaa4944d
Zielony <kamil@dim.pl>
2021-08-24 03:02:11 +0200

    final form of exercise tests and blank classes

--- low-level-api-python/src/main/UserProducer.py
+++ low-level-api-python/src/main/UserProducer.py
@@ -5,17 +5,4 @@ class UserProducer:
 
     def produce(self, bootstrap_servers, topic, users):
 
-        print("topic: " + topic)
-
-        conf = {
-            'bootstrap.servers': bootstrap_servers,
-            'client.id': socket.gethostname()
-        }
-
-        producer = Producer(conf)
-
-        for user in users:
-            print("User produced: " + str(user))
-            producer.produce(topic, key="key", value=str(user))
-
-        producer.flush()
+        return None
