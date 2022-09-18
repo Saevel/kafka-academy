6b3a5b2ef50d08d629e0938b59353770c1bf450c
Kamil Owczarek <kamil@dim.pl>
2022-09-18 23:01:22 +0200

    Improvements in how Python exercises are executed from Gradle

--- low-level-api-python/scripts/setup.sh
+++ low-level-api-python/scripts/setup.sh
@@ -3,6 +3,4 @@ pip install virtualenv
 
 virtualenv ../venv
 
-source ../venv/Scripts/activate
-
-pip install -r ../requirements.txt -t ../venv
+source ../venv/bin/activate && pip install -r ../requirements.txt
